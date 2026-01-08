5110d63e45e3625fdafbaa58772374d584b2e6e1
ian-michael254 <ian.michael.codes@gmail.com>
2026-01-08 15:56:09 +0300

    feat: initialize React project with Vite and Tailwind CSS
    - Added package.json with dependencies for React, ReactDOM, Tailwind CSS, and Vite.
    - Included Vite configuration for React and Tailwind CSS.
    - Created main application entry point (main.jsx) and App component (App.jsx).
    - Added global styles using Tailwind CSS in index.css.
    - Included SVG assets for Vite and React logos.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-center text-4xl font-bold underline text-green-500'>
+      Caroline
+      
+    </div>
+  )
+}
+
+export default App
